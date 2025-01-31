958a45245065262a4f71cf4bd7e18d9fdcd3d2d9
romit shah <romitshah02@gmail.com>
2025-01-31 09:51:16 +0530

    Content Schema

--- backend/db.js
+++ backend/db.js
@@ -23,8 +23,47 @@ const userSchema = new mongoose.Schema({
 
 })
 
+const  ContentSchema = new mongoose.Schema({
+    sections: [
+      {
+        heading: { type: String, required: true },
+        paragraphs: [{ type: String, required: true }]
+      }
+    ]
+});
+
+const BasicsQuizSchema = new mongoose.Schema({
+    question: { type: String, required: true },
+    options: [
+      {
+        type: String,
+        required: true
+      }
+    ],
+    answer: { type: String, required: true }
+  });
+
+const PracticeQuizSchema = new mongoose.Schema({
+    question: { type: String, required: true },
+    options: [
+      {
+        type: String,
+        required: true
+      }
+    ],
+    correctAnswer: { type: Number, required: true },
+    explanation: { type: String, required: true }
+  });
+
+  const Content = mongoose.model('InvestingBasicsContent',ContentSchema);
+  const BasicsQuiz = mongoose.model('InvestingBasicsQuiz', BasicsQuizSchema);
+  const PracticeQuiz = mongoose.model('InvestingPracticeQuiz', PracticeQuizSchema);
+  
 const User = mongoose.model("User", userSchema)
 
 module.exports ={
-    User
+    User,
+    Content,
+    BasicsQuiz,
+    PracticeQuiz
 }
